2a4b7c62ee404843cb7eb360e39400de524f8233
root user <root@domaian.com>
2022-03-24 21:28:26 -0400

    Added RAID to diskconfig.sh

--- diskconfig.sh
+++ diskconfig.sh
@@ -5,3 +5,5 @@ if [ "$1" = "cool" ] then echo "Cool Beans”
 fi
 ## $1 is the first word on the command line
 ## after the command itself
+echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
+mdadm --detail /dev/md0 >>FILENAME.report
